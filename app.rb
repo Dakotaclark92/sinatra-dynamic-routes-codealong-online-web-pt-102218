a3f4645f9eb7da095e1c45e609fddbe735a1023b
Dakota Clark <bannaray95@gmail.com>
2019-03-22 01:04:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
   end
   
   get '/multiply/:num1/:num2' do
-    @number = all_the_numbers.select do |number|
-      number.id == params [:id]
+    @number = params[:num1],[:num2]
+    
     end
     
 
